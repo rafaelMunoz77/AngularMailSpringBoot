d946ba84539635c1a19ddf1878a19f87d8891c78
rafaelMunoz77 <rafael.munoz77@gmail.com>
2020-10-12 12:39:53 +0200

    Vídeo 06 - Inclusión Faceta JPA y entidades

---
+++ src/main/java/com/angularMail/model/entities/TipoSexo.java
@@ -0,0 +1,72 @@
+package com.angularMail.model.entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.List;
+
+
+/**
+ * The persistent class for the tiposexo database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="TipoSexo.findAll", query="SELECT t FROM TipoSexo t")
+public class TipoSexo implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+
+	private String descripcion;
+
+	//bi-directional many-to-one association to Usuario
+	@OneToMany(mappedBy="tipoSexo")
+	@JsonIgnore	
+	private List<Usuario> usuarios;
+
+	public TipoSexo() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescripcion() {
+		return this.descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public List<Usuario> getUsuarios() {
+		return this.usuarios;
+	}
+
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+
+	public Usuario addUsuario(Usuario usuario) {
+		getUsuarios().add(usuario);
+		usuario.setTipoSexo(this);
+
+		return usuario;
+	}
+
+	public Usuario removeUsuario(Usuario usuario) {
+		getUsuarios().remove(usuario);
+		usuario.setTipoSexo(null);
+
+		return usuario;
+	}
+
+}
