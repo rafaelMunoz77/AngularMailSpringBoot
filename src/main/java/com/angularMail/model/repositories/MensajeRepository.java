bd1f9e67ad60f3bd7751c940ec54dcca878e99ad
rafaelMunoz77 <rafael.munoz77@gmail.com>
2020-10-12 15:22:22 +0200

    Vídeo 8 - Más sobre repositorios y servicios de acceso a datos

--- src/main/java/com/angularMail/model/repositories/MensajeRepository.java
+++ src/main/java/com/angularMail/model/repositories/MensajeRepository.java
@@ -1,5 +1,8 @@
 package com.angularMail.model.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +11,8 @@ import com.angularMail.model.entities.Mensaje;
 @Repository
 public interface MensajeRepository extends CrudRepository<Mensaje, Integer> {
 
-
+	@Query(value = "SELECT distinct m.* FROM Mensaje as m, destinatarioMensaje as d "
+			+ "where d.idMensaje = m.id and d.idDestinatario = ? and d.archivado = 0 "
+			+ "and d.spam = 0 and d.fechaEliminacion is null order by m.fecha desc limit ?, ?", nativeQuery = true)
+	public List<Mensaje> getMensajesRecibidosDeUsuarioDesdeCRUDRepository(int idUsuario, int pagina, int elementosPorPagina);
 }
