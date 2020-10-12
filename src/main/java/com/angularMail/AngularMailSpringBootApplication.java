4d8d8c545aa48b97b6e4288aba2f8c18316ec12a
rafaelMunoz77 <32701274+rafaelMunoz77@users.noreply.github.com>
2020-10-12 09:50:26 +0200

    Primer commit

---
+++ src/main/java/com/angularMail/AngularMailSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.angularMail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularMailSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularMailSpringBootApplication.class, args);
+	}
+
+}
