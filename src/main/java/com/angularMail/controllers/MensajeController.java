23364091f528b3b0c0a7dff850700b9160ff24e6
rafaelMunoz77 <rafael.munoz77@gmail.com>
2020-10-13 10:31:33 +0200

    Corrección error en import

--- src/main/java/com/angularMail/controllers/MensajeController.java
+++ src/main/java/com/angularMail/controllers/MensajeController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.angularMail.jwtSecurity.AutenticadorJWT;
 import com.angularMail.model.entities.Mensaje;
 import com.angularMail.model.repositories.MensajeRepository;
 
@@ -18,7 +17,6 @@ public class MensajeController {
 	
 	@GetMapping("/mensajes/recibidos")
 	public List<Mensaje> mensajesRecibidosParaUsuarioAutenticado (int pagina, int mensajesPorPagina, HttpServletRequest request) {
-//		int idUsuAutenticado = AutenticadorJWT.getIdUsuarioDesdeJwtIncrustadoEnRequest(request);
 		return this.mensajeRepo.getMensajesRecibidosDeUsuarioDesdeCRUDRepository(1, pagina, mensajesPorPagina);
 	}
 }
