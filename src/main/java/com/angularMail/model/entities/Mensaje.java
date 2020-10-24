f637c364c669b817e0e8c36247595230750849d5
rafaelMunoz77 <rafael.munoz77@gmail.com>
2020-10-24 11:06:23 +0200

    Inclusión @JsonIgnore

--- src/main/java/com/angularMail/model/entities/Mensaje.java
+++ src/main/java/com/angularMail/model/entities/Mensaje.java
@@ -2,6 +2,9 @@ package com.angularMail.model.entities;
 
 import java.io.Serializable;
 import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.util.Date;
 import java.util.List;
 
@@ -34,11 +37,13 @@ public class Mensaje implements Serializable {
 
 	//bi-directional many-to-one association to DestinatarioMensaje
 	@OneToMany(mappedBy="mensaje")
+	@JsonIgnore
 	private List<DestinatarioMensaje> destinatarioMensaje;
 
 	//bi-directional many-to-one association to Usuario
 	@ManyToOne(fetch=FetchType.LAZY)
 	@JoinColumn(name="idEmisor")
+	@JsonIgnore
 	private Usuario usuarioEmisor;
 
 	public Mensaje() {
@@ -76,23 +81,23 @@ public class Mensaje implements Serializable {
 		this.fecha = fecha;
 	}
 
-	public List<DestinatarioMensaje> getDestinatarioMensajes() {
+	public List<DestinatarioMensaje> getDestinatarioMensaje() {
 		return this.destinatarioMensaje;
 	}
 
-	public void setDestinatarioMensajes(List<DestinatarioMensaje> destinatarioMensajes) {
+	public void setDestinatarioMensaje(List<DestinatarioMensaje> destinatarioMensajes) {
 		this.destinatarioMensaje = destinatarioMensajes;
 	}
 
 	public DestinatarioMensaje addDestinatarioMensaje(DestinatarioMensaje destinatarioMensaje) {
-		getDestinatarioMensajes().add(destinatarioMensaje);
+		getDestinatarioMensaje().add(destinatarioMensaje);
 		destinatarioMensaje.setMensaje(this);
 
 		return destinatarioMensaje;
 	}
 
 	public DestinatarioMensaje removeDestinatarioMensaje(DestinatarioMensaje destinatarioMensaje) {
-		getDestinatarioMensajes().remove(destinatarioMensaje);
+		getDestinatarioMensaje().remove(destinatarioMensaje);
 		destinatarioMensaje.setMensaje(null);
 
 		return destinatarioMensaje;
